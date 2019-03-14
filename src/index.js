8a9e896d1491bacc63ea64a0b8a74281623b6105
Sapar Atayev <sopa.97@mail.ru>
2019-03-14 22:34:29 +0300

    feat: add function advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  var simple=[];
+
+  var p=2;
+  while(base >= p*p) {
+    if(base % p == 0) {
+      simple.push(p);
+      base=base/p;
+    } 
+    else {
+      p++;
+    }
+  }
+  simple.push(base);
+
+  var sums=[];
+
+  for( var i=0; i<simple.length; i++) {
+    var sum=0;
+    var power=1;
+    var simpleNumber=simple[i];
+    while(number/simpleNumber >= 1) {
+      sum += Math.floor(number/simpleNumber);
+      power++;
+      simpleNumber= Math.pow(simple[i],power);
+    }
+    sums.push(sum);
+  }
+
+  var results=[];
+
+  for(var i=0; i<sums.length; i++) {
+    var countSame=0;
+    for(var j=i+1; j<sums.length; j++) {
+      if(sums[i]==sums[j]) countSame++;
+    }
+    results.push(Math.floor(sums[i]/(countSame+1)));
+    i=i+countSame;
+  }
+
+  for(var i=1; i<results.length; i++) {
+    var min=results[i];
+    if(results[i] < min) min=results[i];
+  }
+
+  return min;
 }
